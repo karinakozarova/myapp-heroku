3cbdda23ccbd8be5b371b2392377e3399cde7c32
karinakozarova <karinakozarova1303@gmail.com>
2017-10-09 21:49:38 +0300

    plz work

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -5,12 +5,15 @@ class IntervalsController < ApplicationController
     file = params[:file]
     max_sum = 0
     current_sum = 0
-    CSV.foreach(file.path) do |r|
+
+    CSV.foreach(file.path) do |r| # every row
+    	current_sum = 0
     	30.times do |r| current_sum += r[0]
-    		if current_sum>max_sum
-    			max_sum = current_sum
-    		end
+    	if current_sum>max_sum
+    		max_sum = current_sum
+   		end
  	end
+
     max_sum = result.ceil
     render plain: '%.2f' % max_sum
   end
