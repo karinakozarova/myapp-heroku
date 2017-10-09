185a1ad1bc4a83f8050f321ceea9fbb5ae1ead9c
karinakozarova <karinakozarova1303@gmail.com>
2017-10-09 22:19:07 +0300

    create lin rgression file

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -4,16 +4,23 @@ class IntervalsController < ApplicationController
   def create
     file = params[:file]
     max_sum = 0
+    i = 0
     current_sum = 0
-
+    count = 0 # num of rows
     CSV.foreach(file.path) do |r| # every row
-    	current_sum = 0
-    	30.times do |r| current_sum += r[0]
-    	if current_sum>max_sum
+    	count += 1
+    end 
+    CSV.foreach(file.path) do |r|
+        CSV.foreach(file.path) do |a+r| 
+        	if i == 30 break end
+        	i += 1
+        	current_sum += r[a+r]
+        end
+        if current_sum>max_sum
     		max_sum = current_sum
    		end
- 	end
-
+    end 
+    
     max_sum = result.ceil
     render plain: '%.2f' % max_sum
   end
